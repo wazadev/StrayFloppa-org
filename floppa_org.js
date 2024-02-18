b8193a921ac23d40604221b85957c1d3a1bd306b
firetux <147938644+wazadev@users.noreply.github.com>
2024-02-18 19:53:20 -0300

    Add files via upload

---
+++ floppa_org.js
@@ -0,0 +1,7 @@
+const donateButton = document.querySelector(".button");
+
+donateButton.addEventListener("click", () => {
+  // Reemplaza "XXXXXXXXXXXXXXXXXX" con el ID de tu botón de Mercado Pago
+  // **Para localhost, usa la siguiente URL:**
+  window.location.href = "http://localhost:8080/index.html";
+});
