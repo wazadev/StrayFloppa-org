12cba38c50340d8633cda4a2bb446dc2d32e3763
firetux <147938644+wazadev@users.noreply.github.com>
2024-02-18 19:54:19 -0300

    Update floppa_org.js

--- floppa_org.js
+++ floppa_org.js
@@ -2,6 +2,5 @@ const donateButton = document.querySelector(".button");
 
 donateButton.addEventListener("click", () => {
   // Reemplaza "XXXXXXXXXXXXXXXXXX" con el ID de tu botón de Mercado Pago
-  // **Para localhost, usa la siguiente URL:**
-  window.location.href = "http://localhost:8080/index.html";
+
 });
